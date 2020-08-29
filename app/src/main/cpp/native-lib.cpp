5626ab96df67637cd69f73e2a0226ef07cb886ed
HuaweiCodelab <69961985+HuaweiCodelab@users.noreply.github.com>
2020-08-29 13:50:51 +0800

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,50 +1,43 @@
 #include <jni.h>
 #include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
 #include <fcntl.h>
 #include <android/log.h>
 #include <dispatch/dispatch.h>
 
-#define LOG_TAG "dispatch"
+double calculate_pi(void)
+{
+    __block double pi = 0;
+    int n = 1000000;
+    int threads = 8;
+    dispatch_queue_t accumulator = dispatch_queue_create("Compute pi", NULL);
 
-void dispatch_sample(void);
+    dispatch_apply(threads, DISPATCH_APPLY_AUTO, ^(size_t idx){
+        int start = idx * (n / threads);
+        int end = (idx + 1) * (n / threads);
+        double sum = 0;
+        for (int k = start; k < end; k++) {
+            double flag = (k & 1) ? -1.0 : 1.0;
+            sum += flag / (2 * k + 1);
+        }
+
+        dispatch_sync(accumulator, ^{
+            pi += sum;
+        });
+    });
+
+    dispatch_release(accumulator);
+    return pi * 4;
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_example_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */)
 {
-    std::string hello = "Hello from C++ dispatch!";
-    dispatch_autostat_enable(env);
-    dispatch_sample();
-    return env->NewStringUTF(hello.c_str());
-}
-
-void dispatch_sample(void)
-{
-    int i;
-    dispatch_queue_t concurr_q = dispatch_queue_create("concurrent", DISPATCH_QUEUE_CONCURRENT);
-    dispatch_queue_t serial_q = dispatch_queue_create("serial", DISPATCH_QUEUE_SERIAL);
-
-    for (i = 0; i < 4; i++) {
-        dispatch_async(concurr_q, ^{
-            __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "task %d is running in a concurrent queue", i);
-        });
-    }
-
-    dispatch_barrier_sync(concurr_q, ^{
-        __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "all tasks in the concurrent queue has finished");
-    });
-
-    for (i = 4; i < 8; i++) {
-        dispatch_async(serial_q, ^{
-            __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "task %d is running in a serial queue", i);
-        });
-    }
-
-    dispatch_sync(serial_q, ^{
-        __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "all tasks in the serial queue has finished");
-    });
-
-    dispatch_release(concurr_q);
-    dispatch_release(serial_q);
+    std::stringstream ss;
+    ss << std::setprecision(20) << calculate_pi();
+    return env->NewStringUTF(ss.str().c_str());
 }
